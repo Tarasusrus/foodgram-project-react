8ef21d5ec21850695cbc41eb151f0be0001e634c
tarasusrus <bar.norilsk@gmail.com>
2023-07-11 12:04:19 +0300

    Многострочный Докстринг для permissions.py

--- backend/api/permissions.py
+++ backend/api/permissions.py
@@ -2,6 +2,14 @@ from rest_framework import permissions
 
 
 class IsAuthorOrReadOnly(permissions.BasePermission):
+    """
+    Проверка разрешений: только автор или только чтение.
+
+    - Для методов HTTP, относящихся к безопасным методам (GET, HEAD, OPTIONS), разрешается доступ всем.
+    - Для методов HTTP, отличных от безопасных методов, разрешается доступ только авторизованным пользователям.
+
+    has_object_permission - проверка разрешения для конкретного объекта.
+    """
     def has_permission(self, request, view):
         return (request.method in permissions.SAFE_METHODS
                 or request.user.is_authenticated)
@@ -13,6 +21,12 @@ class IsAuthorOrReadOnly(permissions.BasePermission):
 
 
 class IsAdminOrReadOnly(permissions.BasePermission):
+    """
+    Проверка разрешений: только администратор или только чтение.
+
+    - Для методов HTTP, относящихся к безопасным методам (GET, HEAD, OPTIONS), разрешается доступ всем.
+    - Для методов HTTP, отличных от безопасных методов, разрешается доступ только администраторам.
+    """
     def has_permission(self, request, view):
         return (request.method in permissions.SAFE_METHODS
                 or request.user.is_staff)
