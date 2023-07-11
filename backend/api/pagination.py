7ecb3d1c8104db33bfce6613744b6898c06cfad2
tarasusrus <bar.norilsk@gmail.com>
2023-07-11 12:02:44 +0300

    Многострочный Докстринг для pagination.py

--- backend/api/pagination.py
+++ backend/api/pagination.py
@@ -2,4 +2,10 @@ from rest_framework.pagination import PageNumberPagination
 
 
 class CustumPagination(PageNumberPagination):
+    """
+    Пользовательская пагинация.
+
+    page_size_query_param - параметр запроса для указания количества элементов на странице.
+    """
+
     page_size_query_param = 'limit'
