005461b2492527b41a2325048420fb1d72201e21
tarasusrus <bar.norilsk@gmail.com>
2023-07-11 12:55:37 +0300

    Докстринг flake8 isort black

--- backend/api/validators.py
+++ backend/api/validators.py
@@ -4,18 +4,34 @@ from django.core.exceptions import ValidationError
 
 
 def validate_username(value):
-    '''Валидатор юзера.'''
+    """
+    Валидатор имени пользователя.
 
+    Проверяет, что имя пользователя не является 'user'.
+
+    Аргументы:
+    - value: значение имени пользователя
+
+    Выбрасывает исключение ValidationError, если имя пользователя равно 'user'.
+    """
     if value.lower() == 'user':
         raise ValidationError(
-            ('Имя пользователя не может быть <user>'),
+            ('Имя пользователя не может быть "user"'),
             params={'value': value},
         )
 
 
 def validate_year(value):
-    '''Валидатор даты.'''
+    """
+    Валидатор года.
 
+    Проверяет, что указанный год не превышает текущий год.
+
+    Аргументы:
+    - value: значение года
+
+    Выбрасывает исключение ValidationError, если год больше текущего года.
+    """
     year = dt.date.today().year
     if not (value <= year):
         raise ValidationError('Дата указана некорректно')
@@ -23,11 +39,28 @@ def validate_year(value):
 
 
 def validate_ingredients(self, value):
-    '''Валидатор ингридиентов.'''
+    """
+    Валидатор ингредиентов.
+
+    Проверяет, что список ингредиентов не пуст и
+    что все количества ингредиентов больше 0.
+
+    Аргументы:
+    - value: список ингредиентов
+
+    Выбрасывает исключение ValidationError, если список ингредиентов пуст или
+    содержит некорректные количества.
+
+    Возвращает:
+    Список ингредиентов.
+
+    Пример использования:
+    validate_ingredients(self, value)
+    """
     if not value:
-        raise ValidationError('Добавьте ингридиенты')
+        raise ValidationError('Добавьте ингредиенты')
     for amount in value:
         if amount['amount'] <= 0:
             raise ValidationError(
-                'Колличество ингридиентов должго быть больше 0')
+                'Количество ингредиентов должно быть больше 0')
     return value
