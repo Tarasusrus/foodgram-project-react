3d9ce14d1f0869320d26e7088c6ee950f12ca5f1
tarasusrus <bar.norilsk@gmail.com>
2023-07-11 12:01:30 +0300

    Многострочный Докстринг для filters.py

--- backend/api/filters.py
+++ backend/api/filters.py
@@ -12,22 +12,48 @@ class NameSearchFilter(SearchFilter):
 
 
 class RecipeFilter(rest_framework.FilterSet):
+    """
+    Фильтры для модели Recipe.
+
+    author - фильтрация по автору рецепта.
+    tags - фильтрация по тегам рецепта.
+    is_favorited - фильтрация по избранным рецептам текущего пользователя.
+    is_in_shopping_cart - фильтрация по рецептам, находящимся в корзине покупок текущего пользователя.
+    """
+
     author = rest_framework.ModelChoiceFilter(queryset=User.objects.all())
     tags = rest_framework.AllValuesMultipleFilter(field_name='tags__slug')
     is_favorited = rest_framework.BooleanFilter(method='filter_is_favorited')
-    is_in_shopping_cart = rest_framework.BooleanFilter(
-        method='filter_is_in_shopping_cart')
+    is_in_shopping_cart = rest_framework.BooleanFilter(method='filter_is_in_shopping_cart')
 
     class Meta:
         model = Recipe
         fields = ('author', 'tags', 'is_favorited', 'is_in_shopping_cart')
 
     def filter_is_favorited(self, queryset, name, value):
+        """
+        Фильтрация рецептов по избранным рецептам текущего пользователя.
+
+        queryset - исходный набор данных рецептов.
+        name - имя поля фильтра.
+        value - значение фильтра.
+
+        Возвращает отфильтрованный набор данных рецептов.
+        """
         if value and self.request.user.is_authenticated:
             return queryset.filter(favorites__user=self.request.user)
         return queryset
 
     def filter_is_in_shopping_cart(self, queryset, name, value):
+        """
+        Фильтрация рецептов по рецептам, находящимся в корзине покупок текущего пользователя.
+
+        queryset - исходный набор данных рецептов.
+        name - имя поля фильтра.
+        value - значение фильтра.
+
+        Возвращает отфильтрованный набор данных рецептов.
+        """
         if value and self.request.user.is_authenticated:
             return queryset.filter(shopping__user=self.request.user)
         return queryset
