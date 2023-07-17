65df0edaf7868a15ad1e000314dd201f76794297
tarasusrus <bar.norilsk@gmail.com>
2023-07-17 17:03:20 +0300

    ueryset = User.objects.filter(followers__user=user)

--- backend/api/utils.py
+++ backend/api/utils.py
@@ -25,7 +25,9 @@ from api.serializers import RecipeIngredients
 
 def download_cart(request):
     ingredients = (
-        RecipeIngredients.objects.filter(recipe__shopping_cart__user=request.user)
+        RecipeIngredients.objects.filter(
+            recipe__shopping_cart__user=request.user
+        )
         .values("ingredient__name", "ingredient__measurement_unit")
         .annotate(amount=Sum("amount"))
     )
